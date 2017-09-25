03559956c4692a2f9ac79b669af207a0ed0ae738
bushw011 <bushw011@morris.umn.edu>
2017-09-25 15:04:16 -0500

    Begin writing e2e tests for todo-list page
    This will be particularly helpful for confirming that issues #3, #4, #5, and #6 are working properly

---
+++ client/e2e/todo-list.e2e-spec.ts
@@ -0,0 +1,36 @@
+import {TodoPage} from './todo-list.po';
+import {browser, protractor} from 'protractor';
+
+describe('angular-spark-lab', () => {
+    let page: TodoPage;
+
+    beforeEach(() => {
+        page = new TodoPage();
+    });
+
+    it('should get and highlight User Name attribute ', () => {
+        page.navigateTo();
+        expect(page.getUserTitle()).toEqual('Todo Owner');
+    });
+
+    it('should type something in filer name box and check that it returned correct element', () => {
+        page.navigateTo();
+        page.typeAnOwner("workman");
+        expect(page.getFirstTodo()).toEqual("Workman is in software design");
+    });
+
+    it('should click on the limit 10 times and return 10 elements then ', () => {
+        page.navigateTo();
+        page.limitTodos();
+        for (let i = 0; i < 10; i++) {
+            page.selectUpKey();
+        }
+
+        expect(page.getFirstTodo()).toEqual("Stokes Clayton is 27 years old");
+
+        page.typeAnOwner("Merrill");
+
+        expect(page.getFirstTodo()).toEqual("Merrill Parker is 27 years old");
+
+    });
+});
---
+++ client/e2e/todo-list.po.ts
@@ -0,0 +1,52 @@
+import {browser, element, by} from 'protractor';
+import {Key} from "selenium-webdriver";
+
+export class TodoPage {
+    navigateTo() {
+        return browser.get('/todos');
+    }
+
+    //http://www.assertselenium.com/protractor/highlight-elements-during-your-protractor-test-run/
+    highlightElement(byObject) {
+        function setStyle(element, style) {
+            const previous = element.getAttribute('style');
+            element.setAttribute('style', style);
+            setTimeout(() => {
+                element.setAttribute('style', previous);
+            }, 200);
+            return "highlighted";
+        }
+
+        return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
+    }
+
+    getUserTitle() {
+        let title = element(by.id('title')).getText();
+        this.highlightElement(by.id('title'));
+
+        return title;
+    }
+
+    typeAnOwner(name: string) {
+        let input = element(by.tagName('input'));
+        input.click();
+        input.sendKeys(name);
+    }
+
+    selectUpKey() {
+        browser.actions().sendKeys(Key.ARROW_UP).perform();
+    }
+
+    limitTodos() {
+        let input = element(by.tagName('input'));
+        input.click();
+        input.sendKeys(Key.TAB);
+    }
+
+    getFirstTodo() {
+        let todo = element(by.id('todos')).getText();
+        this.highlightElement(by.id('todos'));
+
+        return todo;
+    }
+}
